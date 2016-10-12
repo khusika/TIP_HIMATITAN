9c109c04aa6e327cb7191015e785e29dfdeb03fd
khusika <khusikadhamar@gmail.com>
2016-10-12 09:18:15 +0700

    Rearrange NavigationDrawer

--- app/src/main/java/com/khusika/himatitan/MainActivity.java
+++ app/src/main/java/com/khusika/himatitan/MainActivity.java
@@ -74,17 +74,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.Home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.ProfilePengurus) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.Event) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.News) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.About) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.Contact) {
+
+        } else if (id == R.id.Team) {
 
         }
 
